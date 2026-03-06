7e19a65559f01963a360468fdba48d7f6df39364
Kron1410 <anmaslen@gmail.com>
2026-03-06 09:38:14 +0400

    Повторная отправка

--- src/pages/offer/components/offer-reviews/offer-reviews.tsx
+++ src/pages/offer/components/offer-reviews/offer-reviews.tsx
@@ -16,13 +16,13 @@ const OfferReviews = memo(({comments}: OfferReviewsPropsType) => {
   return (
     <section className="offer__reviews reviews">
 
-      {comments && (
+      { comments && (
         <>
           <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{comments.length}</span></h2>
           <ReviewsList comments={comments.toSorted(compareComments).slice(0, 10)} />
         </>
       )}
-      {isAuthorized && <CommentForm />}
+      {isAuthorized && <CommentForm /> }
     </section>
   );
 });
