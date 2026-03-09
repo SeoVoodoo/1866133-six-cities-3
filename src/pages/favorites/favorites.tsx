bf93d3906447e1c973d54f2be0561c1083b5964e
Kron1410 <anmaslen@gmail.com>
2026-03-09 12:36:53 +0400

    Переносит fetchfavoriteactions из app в favorites

--- src/pages/favorites/favorites.tsx
+++ src/pages/favorites/favorites.tsx
@@ -1,13 +1,25 @@
-import { useAppSelector } from '../../hooks';
-import { selectFavorites } from '../../store/favorite/favorite.selectors';
+import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { selectFavorites, selectFavoritesStatus } from '../../store/favorite/favorite.selectors';
 import FavoritesEmpty from './components/favorites-empty';
 import FavoritesList from './components/favorites-list';
+import { useSelector } from 'react-redux';
+import { fetchFavoritesAction } from '../../store/favorite/favorite.thunks';
+import { RequestStatus } from '../../const';
 
 
 const Favorites = () => {
 
+  const dispatch = useAppDispatch();
   const favorites = useAppSelector(selectFavorites);
+  const status = useSelector(selectFavoritesStatus);
   const hasFavorites = favorites.length > 0;
+
+  useEffect(() => {
+    if(status === RequestStatus.Success) {
+      dispatch(fetchFavoritesAction());
+    }
+  }, [dispatch, status]);
 
   return (
     <main className={`
